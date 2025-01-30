0dbb3322002bbb5d4cd78297db3b6c39015abcfb
Raghav Kumar <thisisraghavkumar@gmail.com>
2025-01-30 14:36:34 -0500

    Revert "Edited generate private key script to use latest version of lucid"
    This reverts commit 0bc896976fbc239cbdb6e58458631b302e241d66.

--- common/offchain/generate_private_key.ts
+++ common/offchain/generate_private_key.ts
@@ -1,8 +1,8 @@
-import { Lucid, Crypto } from "https://deno.land/x/lucid/mod.ts";
+import { Lucid } from "https://deno.land/x/lucid@0.10.7/mod.ts";
 
-const lucid = new Lucid(undefined, "Preview");
+const lucid = await Lucid.new(undefined, "Preview");
 
-const privateKey = Crypto.generatePrivateKey();
+const privateKey = lucid.utils.generatePrivateKey();
 await Deno.writeTextFile("key.sk", privateKey);
 const address = await lucid
   .selectWalletFromPrivateKey(privateKey)
